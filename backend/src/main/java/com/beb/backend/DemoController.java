e28b49026bd7ff9027ab94f97fc9b8e984d986da
CHAERYEON-LEE <sogogijjang@naver.com>
2024-11-21 23:03:12 +0900

    Feat: 인프라 테스트를 위한 /api/demo 추가

---
+++ backend/src/main/java/com/beb/backend/DemoController.java
@@ -0,0 +1,14 @@
+package com.beb.backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "hi";
+    }
+}
