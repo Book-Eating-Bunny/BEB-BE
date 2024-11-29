702525ba792e94463dae661eb9f5d2916ef2a20e
CHAERYEON-LEE <sogogijjang@naver.com>
2024-11-29 13:56:50 +0900

    Feat: deploy.sh 파일 수정

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,3 @@
-sudo java -jar /home/chaeryeon0402/BEB-BE/backend/build/libs/backend-0.0.1-SNAPSHOT.jar
+sudo nohup java -jar /home/chaeryeon0402/BEB-BE/backend/build/libs/backend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 
 echo "deploy 파일 실행했다"
