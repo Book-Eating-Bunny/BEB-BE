323634950061d1b7c4de560ac08e12bd37b4ded5
joyful-young <isglory22@gmail.com>
2024-12-05 10:44:05 +0900

    Feat: 인증 제공자, UserDetailsService 구현
    - Spring Security 인증 기능 이용을 위해 AuthenticationProvider, UserDetailsService 구현
    - AuthenticationManager 빈 등록
    
    Related to: #20

--- backend/src/main/java/com/beb/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/beb/backend/config/SecurityConfig.java
@@ -1,32 +1,52 @@
 package com.beb.backend.config;
 
+import com.beb.backend.auth.BebAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final Environment env;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
                 .csrf((csrfConfig) -> csrfConfig.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/h2-console/**", "/error",
+                        .requestMatchers("/error",
                                 "/api/v1/users/signup",
+                                "/api/v1/users/login",
                                 "/api/v1/users/email-availability",
-                                "/api/v1/users/nickname-availability").permitAll() // H2 콘솔 허용
+                                "/api/v1/users/nickname-availability").permitAll()
                         .anyRequest().authenticated() // 나머지는 인증 필요
                 )
-                .headers((hc) -> hc.frameOptions((foc) -> foc.disable()));  // H2 콘솔에서 프레임 허용
-        return http.build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//                .addFilterBefore(new JwtValidatorFilter(env), BasicAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // api 로그인: 인증 프로세스를 수동으로 시작하려면 빈을 생성해야.
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        BebAuthenticationProvider authenticationProvider = new BebAuthenticationProvider(userDetailsService, passwordEncoder);
+        return new ProviderManager(authenticationProvider);
     }
 }
