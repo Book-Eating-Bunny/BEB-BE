12d3c3bf2067af8ecc6aabe8df98a287cb88472c
joyful-young <isglory22@gmail.com>
2024-12-10 14:28:05 +0900

    Fix: SecurityConfig CORS 설정 해제
    - CORS 특정 주소만 요청 허용하던 설정을 해제함

--- backend/src/main/java/com/beb/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/beb/backend/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.beb.backend.config;
 
 import com.beb.backend.auth.BebAuthenticationProvider;
 import com.beb.backend.auth.JwtValidatorFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +9,13 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,19 +27,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity
-                .csrf((csrfConfig) -> csrfConfig.disable())
-                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));   // 허용할 IP
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);   // 인증 관련 쿠키를 포함한 요청 허용 여부
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setMaxAge(3600L);            // 브라우저가 CORS 정책을 캐싱하는 시간(초 단위)
-                        return config;
-                    }
-                }))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/error",
                                 "/api/v1/users/signup",
