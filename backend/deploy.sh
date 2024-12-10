a6664af1b2eed740a257badda91bdf2e7b4e31c0
CHAERYEON-LEE <sogogijjang@naver.com>
2024-12-10 09:55:35 +0900

    Feat: deploy.sh 파일 수정

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,5 @@
+cp /home/chaeryeon0402/application.yml /var/lib/jenkins/workspace/backend-pipeline/backend/src/main/resources/application.yml
+
 sudo nohup java -jar /var/lib/jenkins/workspace/backend-pipeline/backend/build/libs/backend-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 
 echo "deploy 파일 실행했다"
