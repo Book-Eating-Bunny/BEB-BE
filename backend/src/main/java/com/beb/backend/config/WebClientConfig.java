ebe78fe17d84b3bda82716d9e232710efa42de87
joyful-young <isglory22@gmail.com>
2024-12-19 17:30:47 +0900

    Feat: 알라딘 API 사용을 위한 WebClient 등록
    - 알라딘 ttbkey 등록
    - 기본 uri 설정
    
    Related to: #27, #41

--- backend/src/main/java/com/beb/backend/config/WebClientConfig.java
+++ backend/src/main/java/com/beb/backend/config/WebClientConfig.java
@@ -1,16 +1,23 @@
 package com.beb.backend.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebClientConfig {
 
     private final Environment env;
+
+    @Value("${open-api.aladin.ttbkey}")
+    private String TTBKEY;
 
     @Bean("naverWebClient")
     public WebClient naverWebClient(WebClient.Builder builder) {
@@ -22,6 +29,9 @@ public class WebClientConfig {
 
     @Bean("aladinWebClient")
     public WebClient aladinWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://www.aladin.co.kr/ttb/api").build();
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory("http://www.aladin.co.kr/ttb/api");
+        factory.setDefaultUriVariables(Map.of("ttbkey", TTBKEY));
+
+        return builder.uriBuilderFactory(factory).build();
     }
 }
