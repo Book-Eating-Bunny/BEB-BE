707babdde9493f7bd70306744da2d419a317e218
joyful-young <isglory22@gmail.com>
2024-12-19 17:35:58 +0900

    Feat: 알라딘 API 응답 수신, 클라이언트에 제공할 응답을 위한 DTO 작성
    - AladinBookSearchResponseDto: 알라딘 응답 기본 구조 중 필요한 필드 수집
    - AladinBookItemDto: 알라딘 상품 정보 응답 기본 구조 중 필요한 필드 수집
    - BookAndUserStatusDto: 클라이언트 측에 제공할 응답 기본 구조
    - BookDetailsDto: 책 상세 정보 DTO
    - UserStatusDto: 책에 대한 유저의 상태 정보 DTO
    
    Related to: #41

---
+++ backend/src/main/java/com/beb/backend/dto/AladinBookSearchResponseDto.java
@@ -0,0 +1,15 @@
+package com.beb.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record AladinBookSearchResponseDto(
+        @NotNull Integer totalResults,
+        @NotNull Integer startIndex,
+        @NotNull Integer itemsPerPage,
+        @NotNull List<AladinBookItemDto> item
+) {
+}
